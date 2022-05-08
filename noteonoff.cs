b09f20c36fe2be501081fc058a6c4e8347fa5230
snikolaj <51296436+snikolaj@users.noreply.github.com>
2022-05-08 20:03:54 +0200

    Update noteonoff.cs

--- noteonoff.cs
+++ noteonoff.cs
@@ -6,6 +6,7 @@ bool[] noteOn = new bool[16];
 int[] commandArray = new int[3];
 
 int index = 0;
+int midiCommandBytes = 3;
 
 for(int i = 0; i < 16; i++)
 {
@@ -13,31 +14,51 @@ for(int i = 0; i < 16; i++)
     noteOn[i] = false;
 }
 
-string midiFunction()
+string midiFunction(int lastByte)
 {
+    // if the byte is > 127, then it is a status byte, where the message begins
+    if(lastByte > 127)
+    {
+        // can be replaced with array or map?
+        switch (lastByte)
+        {
+            case 0x90:
+                midiCommandBytes = 3;
+                break;
+            case 0x80:  
+                midiCommandBytes = 3;
+                break;
+            case 0xD0:
+                midiCommandBytes = 2;
+                break;
+        }
+        commandArray[0] = lastByte;
+        index = 1;
+    }
+    // else it is a data byte, and enter it normally
+    else 
+    {
+        commandArray[index - 1] = lastByte;
+    }
     string response = "";
-    if(index > 2)
+    // if the buffer's index matches the expected number of data bytes from the message call the associated function
+    if (index >= midiCommandBytes)
     {
         switch (commandArray[0])
         {
             case 0x90:
-                if(commandArray[2] == 0)
-                {
-                    response = turnNoteOff();
-                }
-                else
-                {
-                    response = turnNoteOn();
-                }
+                response = turnNoteOn();
                 break;
             case 0x80:
                 response = turnNoteOff();
                 break;
+            case 0xD0:
+                response = afterTouch();
+                break;
         }
-
-        index = 0;
     }
-    
+
+    DebugOut();
     
     return response;
 }
@@ -45,22 +66,31 @@ string midiFunction()
 string turnNoteOn()
 {
     string response = "";
-    for (int i = 0; i < 16; i++)
+    // handle note on with 0 velocity = note off
+    if (commandArray[2] == 0)
     {
-        if (noteOn[i] == false && noteNumber[i] == 0)
-        {
-            noteNumber[i] = commandArray[1];
-            noteOn[i] = true;
-            response = String.Format("Note {0} turned on", commandArray[1]);
-            break;
-        }
+        response = turnNoteOff();
     }
-
-    for (int i = 0; i < 16; i++)
+    else
     {
-        if (noteNumber[i] != 0)
+        for (int i = 0; i < 16; i++)
         {
-            Console.WriteLine("Note {0} {1}", noteNumber[i], (noteOn[i]) ? "on" : "off");
+            // if the associated note position is off, fill it up
+            if (noteOn[i] == false && noteNumber[i] == 0)
+            {
+                noteNumber[i] = commandArray[1];
+                noteOn[i] = true;
+                response = String.Format("Note {0} turned on", commandArray[1]);
+                break;
+            }
+        }
+
+        for (int i = 0; i < 16; i++)
+        {
+            if (noteNumber[i] != 0)
+            {
+                //Console.WriteLine("Note {0} {1}", noteNumber[i], (noteOn[i]) ? "on" : "off");
+            }
         }
     }
 
@@ -84,19 +114,34 @@ string turnNoteOff()
     {
         if (noteNumber[i] != 0)
         {
-            Console.WriteLine("Note {0} {1}", noteNumber[i], (noteOn[i]) ? "on" : "off");
+            //Console.WriteLine("Note {0} {1}", noteNumber[i], (noteOn[i]) ? "on" : "off");
         }
     }
 
     return response;
 }
+string afterTouch()
+{
+    return String.Format("Global aftertouch set to {0}", commandArray[1]);
+}
+void DebugOut()
+{
+    Console.WriteLine("\nDebug out:\nMIDI array: ");
+    for(int i = 0; i < 3; i++)
+    {
+        Console.Write(commandArray[i] + " ");
+    }
+    Console.WriteLine("\nIndex {0} and specific command index {1}", index, midiCommandBytes);
+}
 
+int lastByte;
 
 Console.WriteLine("Hello, World!");
 beginning:
 
-commandArray[index] = int.Parse(Console.ReadLine());
+lastByte = int.Parse(Console.ReadLine());
+
 index++;
-midiFunction();
+Console.WriteLine("\n" + midiFunction(lastByte));
 
 goto beginning;
